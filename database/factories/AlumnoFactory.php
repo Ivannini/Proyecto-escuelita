26f5e17c0b813bc1f573e3282ea36c3abc4a0568
Ivannini <ivan.nudelstejer1301@alumnos.udg.mx>
2025-03-19 16:18:13 -0600

    "Actualizacion de repositorio"

---
+++ database/factories/AlumnoFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Alumno;
+
+class AlumnoFactory extends Factory
+{
+    protected $model = Alumno::class;
+
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->name(),
+            'correo' => $this->faker->unique()->safeEmail(),
+            'fecha_nacimiento' => $this->faker->date(),
+            'ciudad' => $this->faker->city(),
+        ];
+    }
+}
+
